8ca21e3f67d06ce1ac6b64eab6f685b2be3dac7b
Finn Reid <finnreid19@icloud.com>
2018-02-08 22:46:47 -0800

    Initial Commit

---
+++ testing1212/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testing1212
+//
+//  Created by Finn Reid on 2/8/18.
+//  Copyright © 2018 Finn Reid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
